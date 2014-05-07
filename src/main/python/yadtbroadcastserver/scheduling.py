09789acf774b5bb8d5ed1971c8142f6ade7dd70c
mriehl <max@riehl.io>
2014-05-07 15:29:17 +0200

    inline true division equivalent to total_seconds since this is not on py2.6

--- src/main/python/yadtbroadcastserver/scheduling.py
+++ src/main/python/yadtbroadcastserver/scheduling.py
@@ -1,7 +1,10 @@
+from __future__ import division
+
 from datetime import time, datetime, timedelta
 
 
 def seconds_to_midnight(offset_seconds=60):
     now = datetime.now()
     next_midnight = datetime.combine(now, time()) + timedelta(days=1, seconds=offset_seconds)
-    return int((next_midnight - now).total_seconds())
+    datetime_to_midnight = int(next_midnight - now)
+    return (datetime_to_midnight.microseconds + (datetime_to_midnight.seconds + datetime_to_midnight.days * 24 * 3600) * 10**6) / 10**6
