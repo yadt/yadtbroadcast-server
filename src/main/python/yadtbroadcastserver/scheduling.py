0e55270d373b8c54ca403f68af419ae998a8cdad
Valentin Haenel <valentin.haenel@gmx.de>
2014-05-08 09:17:58 +0200

    pep8 whitespace around operator

--- src/main/python/yadtbroadcastserver/scheduling.py
+++ src/main/python/yadtbroadcastserver/scheduling.py
@@ -7,4 +7,4 @@ def seconds_to_midnight(offset_seconds=60):
     now = datetime.now()
     next_midnight = datetime.combine(now, time()) + timedelta(days=1, seconds=offset_seconds)
     datetime_to_midnight = int(next_midnight - now)
-    return (datetime_to_midnight.microseconds + (datetime_to_midnight.seconds + datetime_to_midnight.days * 24 * 3600) * 10**6) / 10**6
+    return (datetime_to_midnight.microseconds + (datetime_to_midnight.seconds + datetime_to_midnight.days * 24 * 3600) * 10 ** 6) / 10 ** 6
