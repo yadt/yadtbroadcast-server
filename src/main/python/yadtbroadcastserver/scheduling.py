86079b967b5a7406f245b656d49a74ab32793962
Arne Hilmann <arne@hilmann.de>
2014-05-08 10:01:32 +0200

    simplify calculation of seconds

--- src/main/python/yadtbroadcastserver/scheduling.py
+++ src/main/python/yadtbroadcastserver/scheduling.py
@@ -1,14 +1,15 @@
 from __future__ import division
 
 from datetime import time, datetime, timedelta
+from math import ceil
 
 
 def _now():
     return datetime.now()
 
 
-def seconds_to_midnight(offset_seconds=60):
+def seconds_to_midnight(offset_seconds=0):
     now = _now()
     next_midnight = datetime.combine(now, time()) + timedelta(days=1, seconds=offset_seconds)
     datetime_to_midnight = next_midnight - now
-    return (datetime_to_midnight.microseconds + (datetime_to_midnight.seconds + datetime_to_midnight.days * 24 * 3600) * 10 ** 6) / 10 ** 6
+    return ceil(datetime_to_midnight.seconds + datetime_to_midnight.days * 24 * 3600)
